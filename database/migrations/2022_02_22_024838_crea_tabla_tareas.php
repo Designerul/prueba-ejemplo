c195ed0c0b405acd372d3bcea632c44325d0c7dc
Designerul <alejandrodiazray01@gmail.com>
2022-02-21 22:28:58 -0600

    crear migraciones

--- database/migrations/2022_02_22_024838_crea_tabla_tareas.php
+++ database/migrations/2022_02_22_024838_crea_tabla_tareas.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('tareas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('tarea');
+            $table->text('descripcion');
+            $table->string('tipo',50);
         });
     }
 
