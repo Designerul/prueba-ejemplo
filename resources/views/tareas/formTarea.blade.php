418e70236b23d3f02d095142be6a1040847f296d
Designerul <alejandrodiazray01@gmail.com>
2022-02-23 22:25:55 -0600

    alerta de errores

--- resources/views/tareas/formTarea.blade.php
+++ resources/views/tareas/formTarea.blade.php
@@ -8,6 +8,17 @@
 </head>
 <body>
     <h1>Agregar Tarea</h1>
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    
     <form action="/tarea" method="POST">
         @csrf
             <label for="tarea">Nombre de la Tarea</label><br>
